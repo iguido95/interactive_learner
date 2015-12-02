217c91d9fa8cb82d60f4f52a1a6888070060947f
Kilian <k.m.ros@student.utwente.nl>
2015-12-02 10:36:26 +0100

    update reader

--- src/TxtReader.java
+++ src/TxtReader.java
@@ -8,30 +8,25 @@ public class TxtReader {
 	TxtReader() {		
 	}
 	
-	public void readTxt(String filepath) throws FileNotFoundException {
+	public ArrayList readTxt(String filepath) throws FileNotFoundException {
 		Scanner s = new Scanner(new File(filepath));
 		ArrayList<String> list = new ArrayList<String>();
 		while (s.hasNext()){
 		    list.add(s.next());
-		    System.out.println(s.next());
+		    System.out.println(s.hasNext());
 		}
 		s.close();
+		return list;
 	}
 	
 	public static void main(String[] args) {
-	
 		TxtReader tr = new TxtReader();
 		try {
-			tr.readTxt("src/F-test1.txt");
+			tr.readTxt("C:\\Development\\eclipse\\workspace\\interactive_learner\\blogs\\F-test1.txt");
 		} catch (FileNotFoundException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
 	}
-	
-	
-	
-	
 	
 }
