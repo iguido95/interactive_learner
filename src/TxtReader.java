9ea84dfe92373c9a8d7eee2b14a2504487c54aef
Kilian <k.m.ros@student.utwente.nl>
2015-12-02 11:59:00 +0100

    TxtReader udpate

--- src/TxtReader.java
+++ src/TxtReader.java
@@ -11,18 +11,53 @@ public class TxtReader {
 	public ArrayList readTxt(String filepath) throws FileNotFoundException {
 		Scanner s = new Scanner(new File(filepath));
 		ArrayList<String> list = new ArrayList<String>();
-		while (s.hasNext()){
+		while (s.hasNext()){	
 		    list.add(s.next());
-		    System.out.println(s.next());
 		}
 		s.close();
 		return list;
 	}
 	
+	//Replaces uppercase letters with lowercase and removes punctuation.
+	public ArrayList normalize(ArrayList<String> list) {
+		ArrayList<String> normalizedList = new ArrayList<String>();
+		
+		for(String word : list) {
+			String normalizedWord = word.toLowerCase();
+			normalizedWord = normalizedWord.replace(",", "");
+			normalizedWord = normalizedWord.replace(".", "");
+			normalizedWord = normalizedWord.replace(";", "");
+			normalizedWord = normalizedWord.replace("!", "");
+			normalizedWord = normalizedWord.replace("?", "");
+			normalizedWord = normalizedWord.replace("(", "");
+			normalizedWord = normalizedWord.replace(")", "");
+			normalizedWord = normalizedWord.replace("{", "");
+			normalizedWord = normalizedWord.replace("}", "");
+			normalizedWord = normalizedWord.replace("[", "");
+			normalizedWord = normalizedWord.replace("]", "");
+			normalizedWord = normalizedWord.replace("<", "");
+			normalizedWord = normalizedWord.replace(">", "");
+			normalizedWord = normalizedWord.replace("%", "");
+			normalizedWord = normalizedWord.replace(" ", "");
+			normalizedWord = normalizedWord.replace("'", "");
+			normalizedWord = normalizedWord.replace("-", "");
+			
+			if (!normalizedWord.equals("")) {
+				normalizedList.add(normalizedWord);
+			}
+		}
+		
+		return normalizedList;
+	}
+	
+	
+	
 	public static void main(String[] args) {
 		TxtReader tr = new TxtReader();
 		try {
-			tr.readTxt("C:\\Development\\eclipse\\workspace\\interactive_learner\\blogs\\F-test1.txt");
+			//System.out.println(tr.readTxt("C:/Development/eclipse/workspace/interactive_learner/blogs/F/F-test1.txt").toString());
+			ArrayList myList = tr.readTxt("C:/Development/eclipse/workspace/interactive_learner/blogs/F/F-test1.txt");
+			System.out.println(tr.normalize(myList).toString());
 		} catch (FileNotFoundException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
