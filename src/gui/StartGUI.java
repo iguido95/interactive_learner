227b28842eb2ca53befb92b15efa6cfa92872f8d
Guido Teunissen <iguido@me.com>
2016-01-03 01:30:21 +0100

    name

--- src/gui/StartGUI.java
+++ src/gui/StartGUI.java
@@ -247,7 +247,7 @@ public class StartGUI extends JPanel {
 	 */
 	private static void createAndShowGUI() {
 		// Create and set up the window.
-		JFrame frame = new JFrame("BoxAlignmentDemo");
+		JFrame frame = new JFrame("Naive Bayesian Classifier");
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
 		// Create and set up the content pane.
